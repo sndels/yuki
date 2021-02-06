5898468afb8190601accd8877e1dac123bd2471e
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2021-02-06 13:52:09 +0200

    Add Mesh

---
+++ yuki/src/shapes/mesh.rs
@@ -0,0 +1,29 @@
+use crate::math::{point::Point3, transform::Transform};
+
+/// Stores the geometry data of a triangle mesh
+pub struct Mesh {
+    pub object_to_world: Transform<f32>,
+    /// Triangle vertex indices stored as triplets
+    pub indices: Vec<usize>,
+    /// Points in world space
+    pub points: Vec<Point3<f32>>,
+}
+
+impl Mesh {
+    /// Creates a new `Mesh`
+    pub fn new(
+        object_to_world: &Transform<f32>,
+        indices: Vec<usize>,
+        mut points: Vec<Point3<f32>>,
+    ) -> Self {
+        for p in &mut points {
+            *p = object_to_world * *p;
+        }
+
+        Self {
+            object_to_world: object_to_world.clone(),
+            indices,
+            points,
+        }
+    }
+}
