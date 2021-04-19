a35eeb95ed3cd707987b20300f97c7f59d109a47
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2021-04-19 18:59:46 +0300

    math: Add coordinate_system

--- yuki/src/math/mod.rs
+++ yuki/src/math/mod.rs
@@ -16,3 +16,17 @@ pub use point::{Point2, Point3};
 pub use ray::Ray;
 pub use transform::Transform;
 pub use vector::{Vec2, Vec3, Vec4};
+
+// From https://www.pbr-book.org/3ed-2018/Geometry_and_Transformations/Vectors#CoordinateSystemfromaVector
+/// Creates perpendicular vectors for `v`.
+///
+/// `v` is expecte to be normalized.
+pub fn coordinate_system<T: common::FloatValueType>(v: Vec3<T>) -> (Vec3<T>, Vec3<T>) {
+    let v1 = if v.x.abs() > v.y.abs() {
+        Vec3::new(-v.z, T::zero(), v.x) / (v.x * v.x + v.z * v.z).sqrt()
+    } else {
+        Vec3::new(T::zero(), v.z, -v.y) / (v.y * v.y + v.z + v.z)
+    };
+    let v2 = v.cross(v1);
+    (v1, v2)
+}
