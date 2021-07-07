e25937ff92b68ce8c3eb9c90f1d4b68f6ee008cf
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2021-07-07 12:42:54 +0300

    material: Parse single-sided diffuse

--- yuki/src/scene/mitsuba/material.rs
+++ yuki/src/scene/mitsuba/material.rs
@@ -16,7 +16,7 @@ pub fn parse<T: std::io::Read>(
     let mut material = Arc::new(Matte::new(Vec3::new(1.0, 0.0, 1.0)));
 
     parse_element!(parser, indent, |name: &OwnedName,
-                                    _: Vec<OwnedAttribute>,
+                                    attributes: Vec<OwnedAttribute>,
                                     level: &mut i32,
                                     _: &mut Option<u32>|
      -> ParseResult<()> {
@@ -26,6 +26,10 @@ pub fn parse<T: std::io::Read>(
                 material = Arc::new(Matte::new(parse_diffuse(parser, indent.clone())?));
                 *level -= 1;
                 indent.truncate(indent.len() - 2);
+            }
+            // TODO: Proper parsing for twosided and similar "recursive" bsdfs so this can come from parse_diffuse
+            "rgb" => {
+                material = Arc::new(Matte::new(parse_rgb(&attributes, "reflectance")?));
             }
             _ => return Err(format!("Unknown material data type '{}'", data_type).into()),
         }
