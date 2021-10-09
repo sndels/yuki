3e911d6a5ebaf12fb42b0bccf4a227da42ef43ed
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2021-10-09 12:58:19 +0300

    fresnel: Add Conductor

--- yuki/src/materials/bsdfs/fresnel.rs
+++ yuki/src/materials/bsdfs/fresnel.rs
@@ -48,3 +48,47 @@ impl Fresnel for Dielectric {
         Spectrum::ones() * (r_parallel * r_parallel + r_perpendicular * r_perpendicular) / 2.0
     }
 }
+
+pub struct Conductor {
+    eta_i: Spectrum<f32>,
+    eta_t: Spectrum<f32>,
+    k: Spectrum<f32>,
+}
+
+impl Conductor {
+    pub fn new(eta_i: Spectrum<f32>, eta_t: Spectrum<f32>, k: Spectrum<f32>) -> Self {
+        Self { eta_i, eta_t, k }
+    }
+}
+
+impl Fresnel for Conductor {
+    fn evaluate(&self, mut cos_theta_i: f32) -> Spectrum<f32> {
+        // TODO: Move to Specturm if this is needed elsewhere
+        fn sqrt(v: Spectrum<f32>) -> Spectrum<f32> {
+            Spectrum::new(v[0].sqrt(), v[1].sqrt(), v[2].sqrt())
+        }
+
+        // pbrt does the abs before calling a helper that does the clamp into [-1,1]
+        cos_theta_i = cos_theta_i.abs().min(1.0);
+        let eta = self.eta_t / self.eta_i;
+        let eta_k = self.k / self.eta_i;
+
+        let cos_theta_i_2 = cos_theta_i * cos_theta_i;
+        let sin_theta_i_2 = 1.0 - cos_theta_i_2;
+        let eta_2 = eta * eta;
+        let eta_k_2 = eta_k * eta_k;
+
+        let t0 = eta_2 - eta_k_2 - sin_theta_i_2;
+        let a_2_plus_b_2 = sqrt(t0 * t0 + eta_2 * eta_k_2 * 4.0);
+        let t1 = a_2_plus_b_2 + cos_theta_i_2;
+        let a = sqrt((a_2_plus_b_2 + t0) * 0.5);
+        let t2 = a * cos_theta_i * 2.0;
+        let rs = (t1 - t2) / (t1 + t2);
+
+        let t3 = a_2_plus_b_2 * cos_theta_i_2 + sin_theta_i_2 * sin_theta_i_2;
+        let t4 = t2 * sin_theta_i_2;
+        let rp = rs * (t3 - t4) / (t3 + t4);
+
+        (rp + rs) * 0.5
+    }
+}
