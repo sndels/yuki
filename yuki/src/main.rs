ea49cef554b15b4c0bd4084b9673690638954120
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2021-12-12 14:02:28 +0200

    Don't use float_interpolation
    Removed from newer nightly

--- yuki/src/main.rs
+++ yuki/src/main.rs
@@ -1,5 +1,4 @@
 #![feature(destructuring_assignment)]
-#![feature(float_interpolation)]
 #![feature(is_sorted)]
 #![warn(clippy::pedantic, clippy::clone_on_ref_ptr)]
 // Might be a good idea to check allowed warnings once in a while
